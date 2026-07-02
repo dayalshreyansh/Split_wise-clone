a93a33f54f3e5640fd97a0afd9c3e5cd9080ccf5
Shreyansh Dayal <shreyansh.dayal0604@gmail.com>
2026-07-02 18:58:09 +0530

    Update CORS for production frontend3

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,19 +19,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "https://split-wise-clone-1-c70a.onrender.com", 
-  "http://localhost:5173" 
-];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", 
   credentials: true,
 }));
 
